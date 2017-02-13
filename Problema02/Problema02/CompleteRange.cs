50d8e1eb14016189ac7ac162d9fb8ce9161f45e0
carloscondorc <carlos.condor@outlook.com>
2017-02-12 23:28:07 -0500

    probiema02

---
+++ Problema02/Problema02/CompleteRange.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema02
+{
+    public class CompleteRange
+    {
+            public int[] build(int[] rango)
+        {
+            List<int> result = new List<int>();
+            int max = rango.Max();
+
+            for (int i = 1; i <= max; i++) {
+                result.Add(i);
+            }
+
+            return result.ToArray();
+        }
+}
